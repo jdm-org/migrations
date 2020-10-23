23c74b0bd851ececcc00561f132b751031e8f259
Anton Kurylionak <anton_kurylionak@epam.com>
2020-10-23 10:14:06 +0300

    new password

--- migrations/11_password_encoding.up.sql
+++ migrations/11_password_encoding.up.sql
@@ -1,5 +1,5 @@
 UPDATE users
-SET password = '$2a$10$vhlxLsj.HcuQVrbRTmOu6OOjTYK/ZeiLTKEA2Sf5JOaxZ98/KHGem'
+SET password = '$2y$10$0PQY075UmNOsU6gPSMwAZexyY.dgEuxIjLvmrfWHIQBP.qBZk0yY.'
 WHERE login = 'superadmin';
 UPDATE users
 SET password = '$2a$10$WS7BhDl4caORMzBQz7y9zeF.iwLr87OipoubI5oo0D7OQ1qIZN8Jm'
